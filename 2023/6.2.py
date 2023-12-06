8a765785024619f885035549e26bc6058cdd6b47
Akash Sadashivapeth <akash.sadashivpeth@gmail.com>
2023-12-05 23:57:21 -0600

    use quadratic formula

--- 2023/6.2.py
+++ 2023/6.2.py
@@ -1,7 +1,6 @@
 import os
 import re
-from functools import reduce
-import operator
+import math
 
 script_dir = os.path.dirname(__file__)
 
@@ -13,18 +12,18 @@ with open(relative_path, 'r') as file:
     for line in file:
         if re.match(r'^Time', line):
             times = re.findall(r'(\d+)', line)
-            times = [int(''.join(times))]
+            times = int(''.join(times))
         elif re.match(r'^Distance', line):
             distance = re.findall(r'(\d+)', line)
-            distance = [int(''.join(distance))]
+            distance = int(''.join(distance))
 
-answers = []
-for time, distance in zip(times, distance):
-    for tick in range(1, int(time)+1):
-        print(tick)
-        epoch_distance = tick * 1 * (time - tick)
-        if epoch_distance > distance:
-            answers.append(epoch_distance)
+# if x = time button in pressed
+# distance travelled = x * (time - x)
+# distance travelled = x*time - x^2
+# x^2 - x*time + distance travelled = 0
+# x = (time +- sqrt(time^2 - 4*distance travelled))/2
 
+x1 = math.floor((times + (times**2 - 4*distance)**0.5)/2)
+x2 = math.ceil((times - (times**2 - 4*distance)**0.5)/2)
 
-print(len(answers))
+print(x1-x2+1)
